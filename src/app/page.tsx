da0481a22df88836015172b84329b6fcd3d4e4f1
Jacob Chademwiri <jchademwiri@gmail.com>
2023-06-15 12:13:14 +0200

    fixed card witdth and title font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,17 @@ import qrcode from '../../public/images/image-qr-code.png';
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col bg-light-gray items-center justify-between p-24'>
-      <section className='bg-white p-4 rounded-lg w-[200px] text-center'>
+      <section className='bg-white p-4 rounded-lg w-[300px] text-center'>
         <Image src={qrcode} alt='qr code' className='rounded-lg' />
-        <h1 className='text-sm font-semibold py-3 text-dark-blue'>
-          Improve your front-end skills by building projects
-        </h1>
-        <p className='text-[15px] text-grayish-blue'>
-          Scan the QR code to visit Frontend Mentor and take your coding skills
-          to the next level
-        </p>
+        <div className='p-2'>
+          <h1 className='text-xl font-semibold py-3 text-dark-blue'>
+            Improve your front-end skills by building projects
+          </h1>
+          <p className='text-[15px] text-grayish-blue'>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
       </section>
       <footer className='attribution'>
         Challenge by{' '}
